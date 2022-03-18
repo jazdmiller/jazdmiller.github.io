a11057bcc036e09ce6658a68013a7c02be73fe05
Jazmine Miller <jazminedmiller@gmail.com>
2022-03-17 20:31:31 -0400

    updated event listeners, added timeout

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
 // cards variables/selectors
-const cards = document.querySelectorAll('.cards')
+// const cards = document.querySelectorAll('.cards')
 
-cards.forEach(card => card.addEventListener('click', flipCard))
+// cards.forEach(card => card.addEventListener('click', flipCard))
 
-function flipCard(e){
-    console.log(e.target);
+// function flipCard(e){
+//     console.log(e.target);
+//     e.target.classList.add('flip');
 
-}
+// }
 
-function shuffleCards(){
+// function shuffleCards(){
 
-}
+// }
 // 
