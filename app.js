0e5cd1a913115601af54ad57032bd89c013bc34b
Jazmine Miller <jazminedmiller@gmail.com>
2022-03-17 22:48:10 -0400

    updated shuffle and reset button

--- app.js
+++ app.js
@@ -1,15 +1,36 @@
-// cards variables/selectors
-// const cards = document.querySelectorAll('.cards')
 
-// cards.forEach(card => card.addEventListener('click', flipCard))
+const cards = document.querySelectorAll('.card')
+let cardFlipped = false;
+let card1, card2;
 
-// function flipCard(e){
-//     console.log(e.target);
-//     e.target.classList.add('flip');
+cards.forEach(card => card.addEventListener('click', flipCard))
 
-// }
+function shuffleCards(){
+    cards.forEach(card => {
+        let randomCard = Math.floor(Math.random() * 12);
+        card.style.order = randomCard;
+    })
+}
+shuffleCards();
 
-// function shuffleCards(){
+function flipCard(){
+    this.classList.toggle('flip');
+    if(cardFlipped === false){
+        cardFlipped = true;
+        card1 = this;
+    } else {
+        cardFlipped = false;
+        card2 = this;
+        if(card1.dataset.name === card2.dataset.name){
+            card1.removeEventListener('click',flipCard);
+            card2.removeEventListener('click',flipCard);
+        } else {
+            setTimeout(() => {
+                card1.classList.remove('flip');
+                card2.classList.remove('flip');
+            }, 1500)
+        }
+    }
+}
 
-// }
-// 
+
